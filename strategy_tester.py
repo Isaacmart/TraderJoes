af6f7a076a4f0f5d4618997e4178ed9afed5dac3
Isaacmart <isaacmartinezjr.mora@gmail.com>
2024-04-20 22:19:32 -0400

    chages to strategy

--- strategy_tester.py
+++ strategy_tester.py
@@ -15,6 +15,7 @@ class Tester:
     def __init__(self, data_path):
         self.data = pd.read_csv(data_path)
         self.trade = 0  # 0 = buy, 1 = sell
+        self.now = ""
         self.current = {}
         self.trades = []
 
@@ -29,26 +30,30 @@ class Tester:
             return ((x['open'] * 100) / self.current.get('entry_price', x['open'])) - 100 < 0
 
         def macd_strategy(x):
+
             if x['shift_hist'] < 0 < x['histogram'] and x['macd'] <= 0 and x['signal'] <= 0 and self.trade == 0:
-                self.trade += 1
                 self.current['entry_time'] = x['time']
-                self.current['entry_price'] = x['open']
+                self.current['entry_price'] = x['close']
                 self.current['entry_macd'] = x['macd']
                 self.current['entry_signal'] = x['signal']
                 self.current['entry_histogram'] = x['histogram']
+
+                self.trade += 1
                 return 0
-            elif (((x['shift_hist'] > 0 > x['histogram'] and x['macd'] >= 0 and x['signal'] >= 0)
-                   or (trade_longer_than(x, 70) and trade_in_red(x)))
-                  and self.trade == 1):
-                self.trade -= 1
+
+            if (((x['shift_hist'] > 0 > x['histogram'] and x['macd'] >= 0 and x['signal'] >= 0)
+                    or (trade_longer_than(x, 70) and trade_in_red(x)))
+                    and self.trade == 1):
                 self.current['exit_time'] = x['time']
-                self.current['exit_price'] = x['open']
+                self.current['exit_price'] = x['close']
                 self.current['exit_macd'] = x['macd']
                 self.current['exit_signal'] = x['signal']
                 self.current['exit_histogram'] = x['histogram']
                 self.current['gain_percentage'] = ((self.current['exit_price'] * 100) / self.current['entry_price']) - 100
                 self.current['duration_in_minutes'] = (self.current['exit_time'] - self.current['entry_time']) / 60
                 self.trades.append(self.current)
+
+                self.trade -= 1
                 self.current = {}
                 return 1
 
